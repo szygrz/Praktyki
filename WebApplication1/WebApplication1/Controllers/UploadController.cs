bf7312d2096a47b4817285ed51eb504475a2fe5f
DESKTOP-53VO3VC\szymo <grzechol0960@wp.pl>
2018-08-02 07:24:42 +0200

    no message

--- WebApplication1/WebApplication1/Controllers/UploadController.cs
+++ WebApplication1/WebApplication1/Controllers/UploadController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,9 +15,47 @@ namespace WebApplication1.Controllers
     public class UploadController : Controller
     {
         // GET: /<controller>/
-        public string Index()
+        private IHostingEnvironment _hostingEnvironment;
+        public UploadController (IHostingEnvironment hostingEnvironment)
         {
-            return "Wrzuciles plik";
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpPost]
+
+        public IActionResult Index(IList <IFormFile> files)
+        {
+          
+            foreach (IFormFile item in files){
+                string filename = ContentDispositionHeaderValue.Parse(item.ContentDisposition).FileName.Trim('"');
+                filename = this.EnsureFilename(filename);
+                using (FileStream filestream = System.IO.File.Create(this.Getpath(filename)))
+                {
+
+                }
+
+            }
+            
+            return this.Content("Dodano plik na serwer");
+           
+        }
+
+        private string Getpath(string filename)
+        {
+            //throw new NotImplementedException();
+            string path = _hostingEnvironment.WebRootPath + "\\Upload\\";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            return path + filename;
+        }
+
+        private string EnsureFilename(string filename)
+        {
+            // throw new NotImplementedException();
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+            return filename;
+                    
         }
     }
 }
+ 
