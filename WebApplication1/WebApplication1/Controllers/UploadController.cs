e9a1a9039491edb863fe6b60cab68be04e0fdc5f
DESKTOP-53VO3VC\szymo <grzechol0960@wp.pl>
2018-08-03 11:24:54 +0200

    no message

---
+++ WebApplication1/WebApplication1/Controllers/UploadController.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WebApplication1.Models.Home;
+
+namespace WebApplication1
+{
+    public class UploadController : Controller
+    {
+        private readonly IFileProvider fileProvider;
+
+        public UploadController(IFileProvider fileProvider)
+        {
+            this.fileProvider = fileProvider;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return Content("files not selected");
+
+            foreach (var file in files)
+            {
+
+                var path = Path.Combine(
+                        Directory.GetCurrentDirectory(),"upload",
+                        file.GetFilename());
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+       
+            return RedirectToAction("Files");
+        }
+        public string Files()
+        {
+            return "This is the Welcome action method...";
+        }
+
+
+    }
+
+    }
+
+     
+
---
+++ WebApplication1/WebApplication1/Models/Home/IFormFileExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Home
+{
+    public static class IFormFileExtensions
+    {
+        public static string GetFilename(this IFormFile file)
+        {
+            return ContentDispositionHeaderValue.Parse(
+                            file.ContentDisposition).FileName.ToString().Trim('"');
+        }
+    }
+}
