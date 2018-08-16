2455095a8e44a5adc4112095b7784bd4fc304fec
DESKTOP-53VO3VC\szymo <grzechol0960@wp.pl>
2018-08-16 14:21:10 +0200

    no message

--- WebApplication1/WebApplication1/Controllers/UploadController.cs
+++ WebApplication1/WebApplication1/Controllers/UploadController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using WebApplication1.Models.Home;
 using Renci.SshNet;
 
+
 namespace WebApplication1
 {
     public class UploadController : Controller
@@ -41,30 +42,41 @@ namespace WebApplication1
                 {
                     await file.CopyToAsync(stream);
                 }
-
                 
             }
 
-            SftpClient client = new SftpClient("10.6.218.22", "Szymon", "qwerty");
-            client.Connect();
-            string localDirectory = @"C:\Users\szymo\Desktop\WebApplication1\WebApplication1\upload";
-            string localPattern = "*.mp3";
-            string ftpDirectory = "/C:/Users/Szymon/Desktop/";
-            string[] files2 = Directory.GetFiles(localDirectory, localPattern);
-            foreach (string file in files2)
+            SftpClient client_sftp = new SftpClient("172.28.115.64", "Szymon", "qwerty");
+            SshClient client_ssh= new SshClient("172.28.115.64", "Szymon", "qwerty");
+            client_sftp.Connect();
+            string localDirectoryUpload = @"C:\Users\szymo\Desktop\WebApplication1\WebApplication1\upload";
+            string localDirectoryDownload = @"C:\Users\szymo\Desktop\WebApplication1\tmp\Scan1.txt";
+            string localPatternUpload = "*.mp3";
+            string uploadDirectory = "/C:/Users/Szymon/Desktop/Upload/";
+            string downloadDirectory= "/C:/Users/Szymon/Desktop/Scan/Scan.txt";
+            string[] filesUpload = Directory.GetFiles(localDirectoryUpload, localPatternUpload);
+            foreach (string file in filesUpload)
             {
                 using (Stream inputStream = new FileStream(file, FileMode.Open))
                 {
-                    client.UploadFile(inputStream, ftpDirectory + Path.GetFileName(file));
-                    
+                    client_sftp.UploadFile(inputStream, uploadDirectory + Path.GetFileName(file));
                 }
             }
+         
+            client_ssh.Connect();
+            client_ssh.RunCommand("C:/Users/Szymon/Desktop/Scan.bat");
+            using (Stream fileStream = System.IO.File.OpenWrite(localDirectoryDownload))
+            {
+                client_sftp.DownloadFile(downloadDirectory, fileStream);
+            }
+
             return RedirectToAction("Files");
         }
         public string Files()
         {
+            
             return "This is the Welcome action method...";
         }
+        
 
 
     }
